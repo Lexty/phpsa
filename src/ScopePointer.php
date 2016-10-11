5a509937cff24c9d96fd718d289076fd3ce8cc76
Medvedev Alexandr <alexandr.mdr@gmail.com>
2016-10-11 13:30:45 +0300

    Make type of \PHPSA\ScopePointer::$object strict

--- src/ScopePointer.php
+++ src/ScopePointer.php
@@ -5,22 +5,28 @@
 
 namespace PHPSA;
 
+use PHPSA\Definition\AbstractDefinition;
+use PHPSA\Definition\ClassDefinition;
 use PHPSA\Definition\ClassMethod;
+use PHPSA\Definition\ClosureDefinition;
 use PHPSA\Definition\FunctionDefinition;
+use PHPSA\Definition\ReflectionClassMethod;
+use PHPSA\Definition\RuntimeClassDefinition;
+use PHPSA\Definition\TraitDefinition;
 
 class ScopePointer
 {
     /**
-     * @var ClassMethod|FunctionDefinition
+     * @var AbstractDefinition
      */
     protected $object;
 
     /**
      * Initializes the scopePointer with an object
      *
-     * @param $object
+     * @param AbstractDefinition $object
      */
-    public function __construct($object)
+    public function __construct(AbstractDefinition $object)
     {
         $this->object = $object;
     }
@@ -46,9 +52,59 @@ class ScopePointer
     }
 
     /**
+     * Is the object a reflection class method?
+     *
+     * @return bool
+     */
+    public function isReflectionClassMethod()
+    {
+        return $this->object instanceof ReflectionClassMethod;
+    }
+
+    /**
+     * Is the object a closure?
+     *
+     * @return bool
+     */
+    public function isClosure()
+    {
+        return $this->object instanceof ClosureDefinition;
+    }
+
+    /**
+     * Is the object a class?
+     *
+     * @return bool
+     */
+    public function isClass()
+    {
+        return $this->object instanceof ClassDefinition;
+    }
+
+    /**
+     * Is the object a runtime class?
+     *
+     * @return bool
+     */
+    public function isRuntimeClass()
+    {
+        return $this->object instanceof RuntimeClassDefinition;
+    }
+
+    /**
+     * Is the object a trait?
+     *
+     * @return bool
+     */
+    public function isTrait()
+    {
+        return $this->object instanceof TraitDefinition;
+    }
+
+    /**
      * Returns the object.
      *
-     * @return ClassMethod|FunctionDefinition
+     * @return AbstractDefinition
      */
     public function getObject()
     {
